d323f9af12f36284ce2690dc3d8020c126b9cf13
Isabelle Sombrio <isabellesombrio@gmail.com>
2025-07-11 16:04:13 -0300

    Add files via upload

--- dommi/src/components/home_1/Home.jsx
+++ dommi/src/components/home_1/Home.jsx
@@ -1,40 +1,71 @@
 import React from 'react'
 import './Home.css'
- <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;600&family=Roboto&display=swap" rel="stylesheet"></link>
+<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;600&family=Roboto&display=swap" rel="stylesheet"></link>
 export default function Home() {
   return (
     <div>
 
-<div className='container_1'>
-      <div className='texto_img'>
-        <p>Solicite um atendimento  AGORA!!</p>
-      </div>
-      <div className='img_salon'>
-        <img src="https://img.freepik.com/vetores-premium/salao-de-cabeleireiro-com-corte-de-cabelo-e-penteado-no-salao-de-beleza-ou-barbeiro-em-ilustracao-plana-dos-desenhos-animados_2175-8670.jpg?semt=ais_items_boosted&w=740" alt="" height="180px" width="220px" />
-      </div>
+      <div className='container_1'>
 
-        <div class="formulario">
-        <input class="endereco" type="text" placeholder="Seu endereço"/>
-        <div class="linha-menores">
-          <input class="data" type="number" placeholder="Data"/>
-          <input class="hora" type="number" placeholder="Horário"/>
+        {/* AGRUPA texto + imagem */}
+        <div className='grupo_texto_img'>
+          <div className='texto_img'>
+            <p>Solicite um <br />
+              atendimento <br /> AGORA!!</p>
+          </div>
+          <div className='img_salon'>
+            <img
+              src="https://img.freepik.com/vetores-premium/salao-de-cabeleireiro-com-corte-de-cabelo-e-penteado-no-salao-de-beleza-ou-barbeiro-em-ilustracao-plana-dos-desenhos-animados_2175-8670.jpg?semt=ais_items_boosted&w=740"
+              alt=""
+              height="140px"
+              width="180px"
+            />
+          </div>
         </div>
-        <button class="agendar">Agendar</button>
+
+        {/* FORMULÁRIO ABAIXO */}
+        <div className="formulario">
+          <input className="endereco" type="text" placeholder="Seu endereço" />
+          <div className="linha-menores">
+            <input className="data" type="number" placeholder="Data" />
+            <input className="hora" type="number" placeholder="Horário" />
+          </div>
+          <button className="agendar">Agendar</button>
+        </div>
       </div>
-</div>
 
+      <br /> <br />
 
-
-
-
-      <div className='carousel'>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-
+      <div className='Carossel'>
+        <div className='texto-carrosel'>
+          <p>Mais procurados</p>
+        </div>
+        <div className='carousel'>
+          <div className="card"> <img src="https://presencadeluxo.pt/img/cms/blog/cabeleireiro%20de%20sucesso.jpg" alt="" width={400} height={200}/></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+        </div>
       </div>
+
+      <div className='Carrosel2'>
+        <div className='texto-carrosel'>
+          <p>Famosos no Dommi</p>
+        </div>
+        <div className='carousel'>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+        </div>
+      </div>
+
+
+
+
+
     </div>
   )
 }
